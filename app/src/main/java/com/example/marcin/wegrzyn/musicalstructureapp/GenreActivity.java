be39ef8255875dcd3f5a090c33bf46ccd70280ad
Marcin <wireamg@gmail.com>
2017-05-01 14:21:04 +0200

    finished app

--- app/src/main/java/com/example/marcin/wegrzyn/musicalstructureapp/GenreActivity.java
+++ app/src/main/java/com/example/marcin/wegrzyn/musicalstructureapp/GenreActivity.java
@@ -1,13 +1,51 @@
 package com.example.marcin.wegrzyn.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class GenreActivity extends AppCompatActivity {
+public class GenreActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String genre = "Genre";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_genre);
+
+        Button selectButton = (Button) findViewById(R.id.selectBTn);
+        Button shareButton = (Button) findViewById(R.id.shareBtn);
+
+        selectButton.setOnClickListener(this);
+        shareButton.setOnClickListener(this);
+    }
+
+    private void startSongActivity() {
+        Intent intent = new Intent(GenreActivity.this,SongsActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void share (String txt){
+
+        Intent implicitIntent = new Intent();
+        implicitIntent.setAction(Intent.ACTION_SEND);
+        implicitIntent.putExtra(Intent.EXTRA_TEXT,txt);
+        implicitIntent.setType("text/plain");
+
+        if (implicitIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(implicitIntent);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id==R.id.selectBTn)
+            startSongActivity();
+        else if (id==R.id.shareBtn)
+            share(genre);
     }
 }
--- app/src/main/java/com/example/marcin/wegrzyn/musicalstructureapp/SongsActivity.java
+++ app/src/main/java/com/example/marcin/wegrzyn/musicalstructureapp/SongsActivity.java
@@ -1,13 +1,52 @@
 package com.example.marcin.wegrzyn.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class SongsActivity extends AppCompatActivity {
+public class SongsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private String song = "Song";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
+
+        Button selectButton = (Button) findViewById(R.id.selectBTn);
+        Button shareButton = (Button) findViewById(R.id.shareBtn);
+
+        selectButton.setOnClickListener(this);
+        shareButton.setOnClickListener(this);
+
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(SongsActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void share (String txt){
+
+        Intent implicitIntent = new Intent();
+        implicitIntent.setAction(Intent.ACTION_SEND);
+        implicitIntent.putExtra(Intent.EXTRA_TEXT,txt);
+        implicitIntent.setType("text/plain");
+
+        if (implicitIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(implicitIntent);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id==R.id.selectBTn)
+            startMainActivity();
+        else if (id==R.id.shareBtn)
+            share(song);
     }
 }
